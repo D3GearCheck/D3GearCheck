99f63e0c68919a54de9b662b9442aae3026cf42b
Jordan Sommers <jtsommer@ucsc.edu>
2015-06-10 09:01:30 -0700

    Quick sorting of items by tier of relevance

--- gear-assignment.js
+++ gear-assignment.js
@@ -9,6 +9,14 @@ var Loadout = {
 		this.skillItems = itemFilter.allItemsForSkillList(className, skillList);
 		this.setItems = itemFilter.setItemsForClass(this.classItems, skillList);
 		console.log(this);
+	},
+	itemListForSlot: function(slot) {
+		var itemList = [];
+		// Quick and dirty, sort by tier
+		itemList = itemList.concat(this.skillItems[slot]);
+		itemList = itemList.concat(this.skillItems[slot]);
+		itemList = itemList.concat(this.classItems[slot]);
+		return itemList;
 	}
 };
 
