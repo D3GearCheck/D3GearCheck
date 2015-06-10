c27e21817559332d9b7fb81be81586bc67b17992
Jordan Sommers <jtsommer@ucsc.edu>
2015-06-09 22:08:09 -0700

    Test snippet for item-filter verification

---
+++ tests/tests.js
@@ -0,0 +1,11 @@
+// These aren't proper tests, more just code snippets used to verify that stuff was working
+
+function itemFilterTest() {
+	var itemFilter = require('./item-filter');
+	var calc = require('./bnet-calc-interface');
+	var skills = calc('http://us.battle.net/d3/en/calculator/barbarian#chkdjP!bhjd!bYZZcZ');
+	var filtered = itemFilter.allItemsForSkillList('barbarian', skills.active);
+	console.log(filtered);
+}
+
+itemFilterTest();
